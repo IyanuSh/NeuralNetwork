0bf7cf9495987b45a6c4e7dfc79adb1c777b004a
Ukonzy <35232812+Ukonzy@users.noreply.github.com>
2022-04-13 20:51:03 -0400

    Latest program

--- ANN Image Classification.py
+++ ANN Image Classification.py
@@ -73,3 +73,4 @@ def process_folder(folder: pathlib.PosixPath) -> pd.DataFrame:
 train_cat = process_folder(folder=pathlib.Path.cwd().joinpath('data/train/cat'))
 train_dog = process_folder(folder=pathlib.Path.cwd().joinpath('data/train/dog'))
 train_set = pd.concat([train_cat, train_dog], axis=0)
+#test
